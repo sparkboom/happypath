1a76dca3577ab748178909cacc44add2a8c29d17
Matthew McLeod <mmcleod@salesforce.com>
2014-05-21 17:27:20 +0100

    start using requirejs

--- scripts/menu-manager.js
+++ scripts/menu-manager.js
@@ -1,17 +1,37 @@
-function loadDependencies() {
-    chrome.tabs.executeScript(null, { file: "scripts/lodash.js" });
-    chrome.tabs.executeScript(null, { file: "scripts/jquery-1.11.0.min.js" });
-    chrome.tabs.executeScript(null, { file: "scripts/queue.js" });
-    chrome.tabs.executeScript(null, { file: "scripts/actions.js" });
-};
+(function (chrome) {
+    'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadDependencies();
+    console.log('menu-manager','module loaded');
 
-    $('a#action1').click(function () {
-        chrome.tabs.executeScript(null, { file: "actions/detailsobjectives.js" });
+    require(['jquery'], function ($) {
+
+        console.log('menu-manager', 'instantiated');
+
+        function loadDependencies() {
+            chrome.tabs.executeScript(null, { file: "../bower_components/jquery/jquery.js" });
+            chrome.tabs.executeScript(null, { file: "../bower_components/angular/angular.js" });
+            chrome.tabs.executeScript(null, { file: "../bower_components/lodash/dist/lodash.js" });
+            chrome.tabs.executeScript(null, { file: "scripts/queue.js" });
+            chrome.tabs.executeScript(null, { file: "scripts/actions.js" });
+        };
+
+        document.addEventListener('DOMContentLoaded', function () {
+            loadDependencies();
+
+            $('a#action1').click(function () {
+                chrome.tabs.executeScript(null, { file: "actions/detailsobjectives.js" });
+            });
+            $('a#action2').click(function () {
+                chrome.tabs.executeScript(null, { file: "actions/enable.js" });
+            });
+        });
+
     });
-    $('a#action2').click(function () {
-        chrome.tabs.executeScript(null, { file: "actions/enable.js" });
-    });
-});
+
+})(chrome);
+
+
+
+
+
+
---
+++ scripts/require-init.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    require.config({
+        baseUrl: 'scripts',
+        paths: {
+            'angular': '../bower_components/angular/angular',
+            'jquery': '../bower_components/jquery/dist/jquery',
+            'lodash': '../bower_components/lodash/dist/lodash'
+        },
+        shim: {
+            angular: {
+                exports: 'angular'
+            },
+            jquery: {
+                exports: '$'
+            },
+            lodash: {
+                exports: '_'
+            }
+        }
+    });
+
+    console.log('require-init');
+
+    require(['jquery','menu-manager'], function($, menuMgr) {
+
+        //start
+        console = chrome.extension.getBackgroundPage().console;
+
+        console.log('test!!');
+
+    });
+
+})();
+
